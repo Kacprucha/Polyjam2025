1260dfb57fc3e4a8ff311ef5b06543dd5c8b7c16
Karaś Kacper <kkaras@acram.eu>
2025-01-24 21:41:00 +0100

    Test tilemaps + walking + coliders

---
+++ Polijem2025/Poligem/Assets/Scripts/Movement/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] float speed = 5f;
+    [SerializeField] Animator animator;
+
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxis ("Horizontal");
+        movement.y = Input.GetAxis ("Vertical");
+
+        animator.SetFloat ("Horizontal", movement.x);
+        animator.SetFloat ("Vertical", movement.y);
+        animator.SetFloat ("Speed", movement.sqrMagnitude);
+    }
+
+    void FixedUpdate ()
+    {
+        rb.MovePosition (rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
