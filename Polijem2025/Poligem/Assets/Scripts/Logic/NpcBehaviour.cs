42057521e8d4b2699b191528fb01fd33eab99f35
Karaś Kacper <kkaras@acram.eu>
2025-01-25 18:52:43 +0100

    fix issues whith walking while talking

--- Polijem2025/Poligem/Assets/Scripts/Logic/NpcBehaviour.cs
+++ Polijem2025/Poligem/Assets/Scripts/Logic/NpcBehaviour.cs
@@ -32,5 +32,6 @@ public class NpcBehaviour : InteractableElement
     {
         interactionKeyFader.FadeOut ();
         isPlayerInRange = false;
+        DialogView.gameObject.SetActive (false);
     }
 }
